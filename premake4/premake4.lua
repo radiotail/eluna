dad4fd6aebeeda9feff16daf9693879acd9f9aa6
radiotail <radiotail86@gmail.com>
2014-11-17 18:05:43 +0800

    add premake4 script
    add premake4 script

---
+++ premake4/premake4.lua
@@ -0,0 +1,34 @@
+solution "ELuna"
+    location("../build")
+	configurations {"Debug", "Release"}
+
+	configuration "Debug"
+  	defines { "DEBUG" }
+	flags {"Symbols"}
+
+	configuration "Release"
+  	defines { "NDEBUG" }
+	flags {"Optimize"}
+
+	project "sample"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir "../bin"
+	includedirs {"../"}
+	includedirs {"../eluna"}
+	
+    files {"../eluna/ELuna.h", "../sample/sample.cpp"}
+	
+	links {"lua51"}
+
+	project "interface"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir "../bin"
+	includedirs {"../"}
+	includedirs {"../eluna"}
+	
+    files {"../eluna/ELuna.h", "../sample/testInterface.cpp"}
+	
+	links {"lua51"}
+
